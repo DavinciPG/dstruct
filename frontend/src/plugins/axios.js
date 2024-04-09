4bb3324e9d0f3272434925bbb4384f8e1f648ccd
Erkz <Erkiparna7@gmail.com>
2024-04-09 16:15:48 +0300

    allow for http requests to our backend

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    withCredentials: true
+    baseURL: 'https://dstruct.vocoprojektid.ee/api',
+    withCredentials: 'include'
 });
 
 const axiosPlugin = {
     install(app) {
-        // Make Axios instance available globally
         app.config.globalProperties.$http = axiosInstance;
     },
 };
